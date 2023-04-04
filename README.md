c0c7f0cb11a6f32147ae25bd366eab15b7f55907
Dmitry S <dmitry.s@hsdn.org>
2023-04-04 20:22:54 +0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# badgui-fix
+
+Fixes the operation of "admincommand" action used in S_ANNOUNCE_UPDATE_NOTIFICATION by replacing them with the built-in browser.
---
+++ index.js
@@ -0,0 +1,33 @@
+const UI = require("../ui");
+const bodyParser = require("../ui/node_modules/body-parser");
+
+module.exports = function GuiFix(mod) {
+	const ui = UI(mod);
+	let title = null;
+	let body = null;
+
+	ui.use(bodyParser.json({ "limit": "50mb" }));
+	ui.use(bodyParser.urlencoded({ "extended": true }));
+	ui.use(UI.static(`${__dirname}/ui`));
+
+	mod.hook("S_ANNOUNCE_UPDATE_NOTIFICATION", 1, { "filter": { "fake": null } }, event => {
+		title = event.title;
+		body = event.body;
+		ui.open();
+		return false;
+	});
+
+	ui.post("/getTitle", (_, response) => {
+		response.json({ title });
+		title = null;
+	});
+
+	ui.post("/getBody", (_, response) => {
+		response.json({ body });
+		body = null;
+	});
+
+	ui.post("/adminCommand", (request) => {
+		mod.send("C_ADMIN", 1, { "command": request.body.command });
+	});
+};
